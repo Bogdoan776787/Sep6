7c47ceb92e466f91306bc8ffa55131811a2428c6
Ion Creciun <33880080+ioncreciun1@users.noreply.github.com>
2022-05-24 16:55:28 +0200

    update APIs

--- infrastructure/module/get_watch_list/app.py
+++ infrastructure/module/get_watch_list/app.py
@@ -14,7 +14,7 @@ def lambda_handler(event, context):
 
     #calling tmdb in order to extract rating
     api_external_key = "2bc24570068939f0e5e7d4182262a186"
-    url = "https://api.themoviedb.org/3/TYPE/MOVIE_ID?api_key=2bc24570068939f0e5e7d4182262a186&language=en-US"
+    url = "https://api.themoviedb.org/3/TYPE/MOVIE_ID?api_key="+ api_external_key+ "&language=en-US"
     query_params = event["queryStringParameters"]
     items = getWatchListTable(query_params["userId"])
 
